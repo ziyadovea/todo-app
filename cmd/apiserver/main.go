7e27e3b504d77016a0f5492c9d0338dac3b924ae
ziyadovea <ziyadovea@gmail.com>
2022-01-21 13:26:57 +0300

    added connect to postgres

--- cmd/apiserver/main.go
+++ cmd/apiserver/main.go
@@ -1,26 +1,46 @@
 package main
 
 import (
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"github.com/ziyadovea/todo-app/internal/app/apiserver"
 	"github.com/ziyadovea/todo-app/internal/pkg/handler"
 	"github.com/ziyadovea/todo-app/internal/pkg/repository"
 	"github.com/ziyadovea/todo-app/internal/pkg/service"
-	"log"
+	"os"
 )
 
 func main() {
 
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+
 	if err := apiserver.InitConfig(); err != nil {
-		log.Fatalf("error occurered while reading configuration file: %s", err.Error())
+		logrus.Fatalf("error occurered while reading configuration file: %s", err.Error())
 	}
 
-	repo := repository.NewRepository()
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalf("error occurered while reading .env file: %s", err.Error())
+	}
+
+	db, err := repository.NewPostgresDB(&repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		User:     viper.GetString("db.user"),
+		DBName:   viper.GetString("db.db_name"),
+		SSLMode:  viper.GetString("db.ssl_mode"),
+		Password: os.Getenv("DB_PASSWORD"),
+	})
+	if err != nil {
+		logrus.Fatalf("error occurered while connecting to the postgres database: %s", err.Error())
+	}
+
+	repo := repository.NewRepository(db)
 	services := service.NewService(repo)
 	h := handler.NewHandler(services)
 
 	server := apiserver.NewServer(viper.GetString("port"), h.InitRoutes())
 	if err := server.Run(); err != nil {
-		log.Fatalf("error occurered while running http server: %s", err.Error())
+		logrus.Fatalf("error occurered while running http server: %s", err.Error())
 	}
 }
--- internal/pkg/repository/repository.go
+++ internal/pkg/repository/repository.go
@@ -1,5 +1,7 @@
 package repository
 
+import "github.com/jmoiron/sqlx"
+
 type Authorization interface {
 }
 
@@ -15,7 +17,7 @@ type Repository struct {
 	TodoItem
 }
 
-func NewRepository() *Repository {
+func NewRepository(db *sqlx.DB) *Repository {
 	return &Repository{
 		Authorization: nil,
 		TodoList:      nil,
